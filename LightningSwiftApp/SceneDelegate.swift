20a0e8cd0b75c8322fe58e6a97a6a6356011c556
bejitono <stdemicheli@protonmail.com>
2019-12-09 21:36:57 +0700

    Initial commit.

---
+++ LightningSwiftApp/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  LightningSwift
+//
+//  Created by De MicheliStefano on 09.12.19.
+//  Copyright © 2019 De MicheliStefano. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    }
+
+}
+
