52ae49c57e2bd63bee148ae32a4be770f9cce573
bejitono <stdemicheli@protonmail.com>
2019-12-14 17:49:42 +0700

    Moves LightningSwift model files.

---
+++ LightningSwift/Contract/Model/LNSWallet/LNSSeed.swift
@@ -0,0 +1,9 @@
+//
+//  LNSSeed.swift
+//  LightningSwift
+//
+//  Created by De MicheliStefano on 14.12.19.
+//  Copyright © 2019 De MicheliStefano. All rights reserved.
+//
+
+import Foundation
---
+++ LightningSwift/Contract/Model/LNSWallet/LNSSeedConfiguration.swift
@@ -0,0 +1,9 @@
+//
+//  LNSSeedConfiguration.swift
+//  LightningSwift
+//
+//  Created by De MicheliStefano on 14.12.19.
+//  Copyright © 2019 De MicheliStefano. All rights reserved.
+//
+
+import Foundation
