1318672bf8f539c70b1d4e064cbc015067446b17
bejitono <stdemicheli@protonmail.com>
2019-12-14 18:33:14 +0700

    Defines wallet init contract.

--- LightningSwift/Contract/Module/LNSWallet/LNSWallet.swift
+++ LightningSwift/Contract/Module/LNSWallet/LNSWallet.swift
@@ -6,8 +6,9 @@
 //  Copyright © 2019 De MicheliStefano. All rights reserved.
 //
 
-public typealias LNSInitWalletCompletion = (Result<Bool, Error>) -> Void
 public typealias LNSSeedCompletion = (Result<LNSSeed, Error>) -> Void
+public typealias LNSInitWalletCompletion = (Result<Bool, Error>) -> Void
+public typealias LNSUnlockWalletCompletion = (Result<Bool, Error>) -> Void
 
 public protocol LNSWallet {
     
@@ -15,9 +16,11 @@ public protocol LNSWallet {
     
     func generateSeed(completion: @escaping LNSSeedCompletion)
     
-    func generateSeed(withConfig: LNSSeedConfiguration, completion: @escaping LNSSeedCompletion)
-}
-
-enum LNSWalletError: Error {
-    case alreadyCreated
+    func generateSeed(withConfig config: LNSSeedConfiguration, completion: @escaping LNSSeedCompletion)
+    
+    func initWalletWith(password: String, seed: LNSSeed, completion: @escaping LNSInitWalletCompletion)
+    
+    func unlockWallet(withPassword password: String, completion: @escaping LNSUnlockWalletCompletion)
+    
+    func changeWallet(password: String, to newPassword: String)
 }
---
+++ LightningSwift/Contract/Module/LNSWallet/LNSWalletError.swift
@@ -0,0 +1,11 @@
+//
+//  LNSWalletError.swift
+//  LightningSwift
+//
+//  Created by De MicheliStefano on 14.12.19.
+//  Copyright © 2019 De MicheliStefano. All rights reserved.
+//
+
+enum LNSWalletError: Error {
+    case alreadyCreated
+}
