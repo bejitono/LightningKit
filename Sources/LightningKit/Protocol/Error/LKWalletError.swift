dae77708c8ed38b98b4eafd36db98be1146e7b11
bejitono <stdemicheli@protonmail.com>
2020-06-07 13:27:28 +0700

    Defines wallet errors.

---
+++ Sources/LightningKit/Protocol/Error/LKWalletError.swift
@@ -0,0 +1,12 @@
+//
+//  LKWalletError.swift
+//  LightningKit
+//
+//  Created by De MicheliStefano on 07.06.20.
+//  Copyright © 2020 De MicheliStefano. All rights reserved.
+//
+
+public enum LKWalletError: Error {
+    case invalidPassword
+    case seedNotGenerated
+}
