76a6ae726cc62b5522543a2b5af1c44f2dfa7423
bejitono <stdemicheli@protonmail.com>
2020-06-16 07:25:23 +0700

    Implements UIView transform extension.

---
+++ Sources/LightningKitUI/View/Button.swift
@@ -0,0 +1,33 @@
+//
+//  Button.swift
+//  LightningKitApp
+//
+//  Created by De MicheliStefano on 16.06.20.
+//  Copyright © 2020 De MicheliStefano. All rights reserved.
+//
+
+import UIKit
+
+open class Button: UIButton {
+    
+    
+}
+
+extension UIView {
+    
+    func applyTransform(
+        isHighlighted: Bool,
+        animationDuration: TimeInterval = 0.1,
+        resizeScale: CGFloat = 0.95
+    ) {
+        if isHighlighted {
+            UIView.animate(withDuration: animationDuration) {
+                self.transform = CGAffineTransform(scaleX: resizeScale, y: resizeScale)
+            }
+        } else {
+            UIView.animate(withDuration: animationDuration) {
+                self.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }
+        }
+    }
+}
