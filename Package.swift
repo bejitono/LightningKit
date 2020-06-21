b0bdefe755133c08a835e588d686698df6af8536
bejitono <48912582+bejitono@users.noreply.github.com>
2020-06-21 16:57:32 +0700

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,10 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "LightningKit",
-            targets: ["LightningKit", "LightningKitUI"]),
+            targets: ["LightningKit"]),
+        .library(
+            name: "LightningKitUI",
+            targets: ["LightningKitUI"),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
