8f31365a620f9f78d8597c38e0e9641fe8495715
bejitono <48912582+bejitono@users.noreply.github.com>
2020-06-21 16:52:46 +0700

    Updates Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "LightningSwift",
+    name: "LightningKit",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "LightningSwift",
-            targets: ["LightningSwift"]),
+            name: "LightningKit",
+            targets: ["LightningKit", "LightningKitUI"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "LightningSwift",
+            name: "LightningKit",
             dependencies: []),
-        .testTarget(
-            name: "LightningSwiftTests",
-            dependencies: ["LightningSwift"]),
+        .target(
+            name: "LightningKitUI",
+            dependencies: ["LightningKit"]),
     ]
 )
