e02995a778d9c8e18520ab8f4217a9a0be87166f
bejitono <stdemicheli@protonmail.com>
2020-06-21 12:26:42 +0700

    Adds btc unit model.

---
+++ Sources/LightningKitClient/Protocol/Model/BTCUnit.swift
@@ -0,0 +1,13 @@
+//
+//  BTCUnit.swift
+//  LightningKitApp
+//
+//  Created by De MicheliStefano on 21.06.20.
+//  Copyright © 2020 De MicheliStefano. All rights reserved.
+//
+
+public enum BTCUnit {
+    case btc(Double)
+    case satoshi(Int)
+    case mSatoshi(Int)
+}
