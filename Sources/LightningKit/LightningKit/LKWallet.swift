e8bff4c1ca1e435404c96592856096279bd35c8e
bejitono <stdemicheli@protonmail.com>
2020-07-18 14:00:54 +0700

    Implements seedless wallet creation.

--- Sources/LightningKit/LightningKit/LKWallet.swift
+++ Sources/LightningKit/LightningKit/LKWallet.swift
@@ -18,22 +18,27 @@ open class LKWallet {
     
     public var unlocked: Bool = false
     
-    private var seed: Seed?
+    private let seedStore: SeedStore
     private let client: LNSCoreService
     
-    public init(client: LNSCoreService = LightningKitClient.shared) {
-        self.client = client
+    public convenience init(client: LNSCoreService = LightningKitClient.shared) {
+        self.init(client: client)
         // TODO: Add config: seedless, no password
+    }
+    
+    init(client: LNSCoreService,
+         seedStore: SeedStore = SeedStoreImpl()) {
+        self.client = client
+        self.seedStore = seedStore
     }
     
     // MARK: - Wallet creation
     
     /// Generates a seed with which users can recover their private keys.
     open func generateSeed(completion: @escaping (Result<Seed, Error>) -> Void) {
-        client.wallet.generateSeed { [weak self] result in
+        client.wallet.generateSeed { result in
             switch result {
             case .success(let seed):
-                self?.seed = seed.phrase
                 completion(Result.success(seed.phrase))
             case .failure(let error):
                 completion(Result.failure(error))
@@ -41,25 +46,68 @@ open class LKWallet {
         }
     }
     
-    /// Creates a new wallet without a password. The password will be internally generated and saved on the device.
+    /// Creates a new wallet without a password. The password will be internally generated and saved to the Keychain.
+    /// A seed will be generated and saved to the Keychain.
     open func create(completion: @escaping (Result<Void, Error>) -> Void) {
-        return completion(Result.failure(LKError.notImplemented))
+        // TODO: Generate password and store in keychain
+        let password = "12345678"
+        
+        client.wallet.generateSeed { [weak self] result in
+            switch result {
+            case .success(let seed):
+                do {
+                    try self?.seedStore.set(seed.phrase) // Enciphered seed instead?
+                    self?.createWith(password: password, seed: seed.phrase) { result in
+                        switch result {
+                        case .success(_): completion(Result.success(()))
+                        case .failure(let error): completion(Result.failure(error))
+                        }
+                    }
+                } catch {
+                    completion(Result.failure(error))
+                }
+            case .failure(let error):
+                completion(Result.failure(error))
+            }
+        }
+    }
+    
+    /// Creates a new wallet without a password. Password has to be minimum 8-digits long.
+    /// A seed will be generated and saved to the Keychain.
+    /// - Parameters:
+    ///   - password: 8-digit password
+    open func createWith(password: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        client.wallet.generateSeed { [weak self] result in
+            switch result {
+            case .success(let seed):
+                do {
+                    try self?.seedStore.set(seed.phrase) // Enciphered seed instead?
+                    self?.createWith(password: password, seed: seed.phrase) { result in
+                        switch result {
+                        case .success(_): completion(Result.success(()))
+                        case .failure(let error): completion(Result.failure(error))
+                        }
+                    }
+                } catch {
+                    completion(Result.failure(error))
+                }
+            case .failure(let error):
+                completion(Result.failure(error))
+            }
+        }
     }
     
     /// Creates a new wallet without a password. Password has to be minimum 8-digits long.
     /// - Parameters:
     ///   - password: 8-digit password
-    open func create(password: String, completion: @escaping (Result<Void, Error>) -> Void) {
+    ///   - seed: Seed generated with generateSeed(completion:)
+    open func createWith(password: String, seed: [String], completion: @escaping (Result<Void, Error>) -> Void) {
         guard !client.ready else {
             return completion(Result.failure(LKError.lightningNodeNotReady))
         }
         
         guard password.count > Constants.minPasswordLength else {
             return completion(Result.failure(LKWalletError.invalidPassword))
-        }
-        
-        guard let seed = seed else {
-            return completion(Result.failure(LKWalletError.seedNotGenerated))
         }
         
         client.wallet.initializeWith(password: password, seed: seed) { result in
