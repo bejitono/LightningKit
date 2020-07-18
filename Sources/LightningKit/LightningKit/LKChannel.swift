9734bf2c5ef3e87ac6ef8a39c13ac72138f52dae
bejitono <stdemicheli@protonmail.com>
2020-07-18 14:24:56 +0700

    Implements get info.

--- Sources/LightningKit/LightningKit/LKChannel.swift
+++ Sources/LightningKit/LightningKit/LKChannel.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2020 De MicheliStefano. All rights reserved.
 //
 
-import Foundation
+import LightningKitClient
+
+open class LKChannel {
+    
+    private let client: LNSCoreService
+    
+    public init(client: LNSCoreService = LightningKitClient.shared) {
+        self.client = client
+        // TODO: Add config: seedless, no password
+    }
+    
+    /// Generates a seed with which users can recover their private keys.
+    open func getInfo(completion: @escaping (Result<LNSInfo, Error>) -> Void) {
+        client.getInfo { (result) in
+            switch result {
+            case .success(let info):
+                completion(Result.success(info))
+            case .failure(let error):
+                completion(Result.failure(error))
+            }
+        }
+    }
+}
